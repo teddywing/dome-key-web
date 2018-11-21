d924cca39802b2629312a5629625ce41f664b2b3
Teddy Wing <git@teddywing.com>
2018-11-21 20:48:42 +0100

    Add license (mostly GNU AGPLv3+)
    License the software with the GNU AGPLv3+ with the exception of the
    'aquatic-prime' and 'paddle' libraries and 'aquatic-prime' binary, which
    are licensed under the GNU GPLv3+.

--- license-generator/src/zip.rs
+++ license-generator/src/zip.rs
@@ -1,3 +1,21 @@
+// Copyright (c) 2018  Teddy Wing
+//
+// This file is part of DomeKey Web.
+//
+// DomeKey Web is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DomeKey Web is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with DomeKey Web. If not, see
+// <https://www.gnu.org/licenses/>.
+
 use std::io::{Seek, Write};
 
 use zip_lib as zip;
